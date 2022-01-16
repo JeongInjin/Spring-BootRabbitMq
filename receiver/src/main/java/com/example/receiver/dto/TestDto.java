b5f338b19d7e92b96814513e8d10a4ea7c0ba9f4
injinjeong <snowwwwww@naver.com>
2022-01-16 23:39:13 +0900

    rabbitMQ basic  commit

---
+++ receiver/src/main/java/com/example/receiver/dto/TestDto.java
@@ -0,0 +1,15 @@
+package com.example.receiver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestDto {
+    Integer no;
+    String name;
+    String loc;
+    String time;
+}
---
+++ sender/src/main/java/com/example/sender/dto/Dept.java
@@ -0,0 +1,17 @@
+package com.example.sender.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dept {
+    Integer deptNo;
+    String dName;
+    String loc;
+    String time;
+
+}
